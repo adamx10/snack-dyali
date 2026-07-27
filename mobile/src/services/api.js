831e302e36d918f6531745eecc93e5552bcf3bb9
adam <hazimaa1254@gmail.com>
2026-07-27 11:13:21 +0100

    fix the probleme

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL :"http://172.19.208.1:5000/api",
+    baseURL :"http://192.168.1.63:5000/api",
 //   baseURL: "http://localhost:5000/api",
 
   headers: {
